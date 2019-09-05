f3e81ea68edf7887f31d4745ea81726a31a848bb
Josélia L. Costa <joselialcosta@gmail.com>
2019-09-04 22:41:27 -0300

    Create basic header and main tags

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>O lanche chegou?</h1>
       </header>
+      <main>
+        <h2>Não</h2>
+      </main>
     </div>
   );
 }
